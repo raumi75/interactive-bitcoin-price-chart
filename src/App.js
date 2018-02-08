116272b8fb00df59e1c398909fd3fba855440e8f
Raumi75 <@raumi75>
2018-02-08 15:31:43 +0100

    FIXED: Formula and Date ADDED: Explanation

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   componentDidMount(){
     const mcAfeeStartDate = '2017-07-17'; // Date of Tweet
     const getData = () => {
-      const url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start='+mcAfeeStartDate+'&end=2020-02-07';
+      const url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start='+mcAfeeStartDate+'&end=2020-12-31';
 
       fetch(url).then( r => r.json())
         .then((bitcoinData) => {
@@ -55,9 +55,15 @@ class App extends Component {
 
   // USD/BTC according to John McAfee's Tweet (1.000.000 by 2020)
   getMcAfeeRate(s){
-    const goalRate = 0.88297529; // Growth Rate to goal of 1.000.000 USD/BTC
+    const goalRate = 1+this.getGrowthRate(); // daily growth rate to goal of 1.000.000 USD/BTC
     const tweetBPI = 2244.265;   // start rate USD/BTC at day of tweet
-    return Math.round(Math.pow(10, goalRate * (s/365)) * tweetBPI);
+    return Math.round(Math.pow(goalRate, s) * tweetBPI);
+  }
+
+  getGrowthRate() { return 0.00484095703431026; }
+
+  getDaysSincePrediction(d) {
+    return moment(d).diff(moment('2017-07-17'),'days')
   }
 
   render() {
@@ -68,7 +74,7 @@ class App extends Component {
           <h1>McAfee Tracker</h1>
         </div>
         <div className='row'>
-          <p>Will Bitcoin be worth 1 Million Dollars on 2020-07-17 or will John McAfee eat his own dick?</p>
+          <p>Will Bitcoin be worth 1 Million Dollars on 2020-12-31 or will John McAfee eat his own dick?</p>
         </div>
 
         <div className='row'>
@@ -92,11 +98,21 @@ class App extends Component {
 
         <div className='row'>
           <p id="explanation">
-            John McAfee made a bet on Juli 17th 2017: One single Bitcoin would be worth 500.000 USD in three years. The price was 2,244.265 USD/BTC. He later revised it <a href="https://twitter.com/officialmcafee/status/935900326007328768">and predicted one Million</a>.
-
-            Is this really possible? Bitcoin needs to grow at a rate of <strong>0.88297529 % per day</strong>. That is the red line on the above chart. As long as the blue line is above the red line, we are on target and John McAfee will not have to eat his own dick.
-
-            Hover over the graph to get daily prices.
+            John McAfee made a bet on Juli 17th 2017: One single Bitcoin would be worth 500.000 US$ in three years. The price was 2,244.265 US$/BTC. He later revised his bet and <a href="https://twitter.com/officialmcafee/status/935900326007328768">predicted one Million US$ by the end of 2020</a>.
+            <br />
+            <br />Is this really possible? Bitcoin needs to grow at a rate of <strong>{ this.getGrowthRate()*100 } % per day</strong>. That is the red line on the above chart. As long as the blue line is above the red line, we are on target and John McAfee will not have to eat his own dick.
+            <br />
+            <br />The growth rate of less than half a percent does not sound like much to you? That´s because we all suck at grasping the concept of exponential growth. This is the magic behind compound interest.
+            <br />
+            <br />Grab a calculator and try it yourself:
+            <br />
+            <br />Today, the prediction is { this.getDaysSincePrediction(Date.now()) } days old, so the target-price is
+            <br />{1+this.getGrowthRate()}<sup><strong>{this.getDaysSincePrediction(Date.now())}</strong></sup> * 2,244.265 $ = { this.getMcAfeeRate(this.getDaysSincePrediction(Date.now())).toLocaleString('us-EN',{ style: 'currency', currency: 'USD' }) }
+            <br />
+            <br />By 2020-12-31, the prediction will be {this.getDaysSincePrediction('2020-12-31')} days old
+            <br />{1+this.getGrowthRate()}<sup><strong>{this.getDaysSincePrediction('2020-12-31')}</strong></sup> * 2,244.265 $ = { this.getMcAfeeRate(this.getDaysSincePrediction('2020-12-31')).toLocaleString('us-EN',{ style: 'currency', currency: 'USD' }) }
+            <br />
+            <br />Hover over the graph to get daily prices.
           </p>
         </div>
 
