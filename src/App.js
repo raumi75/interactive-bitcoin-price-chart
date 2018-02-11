52dbb923c87f6425590511f782825e1930eebda7
Raumi75 <@raumi75>
2018-02-11 16:43:45 +0100

    CHANGED: Improved Layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import { Grid, Row , Col, Image } from 'react-bootstrap';
+import { Grid, Row , Col, Image, PageHeader } from 'react-bootstrap';
 import './App.css';
 import LineChart from './LineChart';
 import ToolTip from './ToolTip';
@@ -90,10 +90,10 @@ class App extends Component {
 
         <Row>
           <Col xs={12}>
-            <h1>bircoin.top</h1>
-            <h2>The McAfee Prediction Tracker</h2>
-            <p>Bitcoiners turn typos into jargon. So HODL on tight to your BIRCOIN! By the end of 2020 it will be worth $1 Million.</p>
-            <p className="redlineExplanation">The red line steadily grows to 1 Mio $/BTC.</p>
+            <PageHeader>
+              bircoin.top <small>The McAfee Prediction Tracker</small>
+            </PageHeader>
+            <p>Bitcoiners turn typos into jargon. So HODL on tight to your BIRCOIN! John McAfee says: By the end of 2020 it will be worth $1 Million.</p>
           </Col>
         </Row>
 
@@ -110,6 +110,7 @@ class App extends Component {
             {this.state.hoverLoc ? <ToolTip hoverLoc={this.state.hoverLoc} activePoint={this.state.activePoint}/> : null}
           </div>
         </Row>
+
         <Row>
           <div className='chart'>
             { !this.state.fetchingData ?
@@ -120,6 +121,7 @@ class App extends Component {
 
         <Row>
           <Col xs={12}>
+          <p className="redlineExplanation">The red line steadily grows to 1 Mio $/BTC.</p>
             <p id="coindesk"> Powered by <a href="http://www.coindesk.com/price/">CoinDesk</a></p>
             <p id="acknowledgement"> Based on Brandon Morellis <a href="https://codeburst.io/how-i-built-an-interactive-30-day-bitcoin-price-graph-with-react-and-an-api-6fe551c2ab1d">30 Day Bitcoin Price Graph</a></p>
           </Col>
@@ -127,22 +129,20 @@ class App extends Component {
 
 
         <Row>
-          <Col xs={12}>
-
-          <p className="explanation">
-            John McAfee made a bet on July 17th 2017: One single Bitcoin would be worth 500.000 US$ in three years. The price was {tweetPrice.toLocaleString('us-EN',{ style: 'currency', currency: 'USD', minimumFractionDigits: 3 }) } at the time. He later revised his bet and <a href="https://twitter.com/officialmcafee/status/935900326007328768">predicted one Million US$ by the end of 2020</a>.
-          </p>
+          <Col xs={12} md={6}>
+            <h2>It started with a tweet</h2>
+            <p className="explanation">
+              John McAfee made a bet on July 17th 2017: One single Bitcoin would be worth 500.000 US$ in three years. The price was {tweetPrice.toLocaleString('us-EN',{ style: 'currency', currency: 'USD', minimumFractionDigits: 3 }) } at the time. He later revised his bet and <a href="https://twitter.com/officialmcafee/status/935900326007328768">predicted one Million US$ by the end of 2020</a>.
+            </p>
           </Col>
-        </Row>
-
-        <Row>
-          <Col xs={12}>
+          <Col xs={12} md={6}>
             <p><a href="https://twitter.com/officialmcafee/status/935900326007328768"><Image src="tweet20171129.png" responsive /></a></p>
           </Col>
         </Row>
 
         <Row>
           <Col xs={12}>
+            <h2>The Math behind it</h2>
 
             <p>Is this really possible? Bitcoin needs to grow at a rate of <strong>{ growthRate*100 } % per day</strong>. That is the red line on the above chart. As long as the blue line is above the red line, we are on target and John McAfee will not have to eat his own dick. Hover over the graph to get daily prices.</p>
             <p>The growth rate of less than half a percent does not sound like much to you? That´s because we all suck at grasping the concept of exponential growth. This is the magic behind compound interest.</p>
@@ -156,9 +156,15 @@ class App extends Component {
             <p>{this.explainPriceOn('2020-06-01')}</p>
             <p>And finally, {this.explainPriceOn('2020-12-31')}</p>
 
-            <p>Of course, this growth has limits, but remember that there will only be 21 Million BTC. If every Millionaire in the world wants one, there are not enough for every one to have a whole BTC.</p>
-            <p>It will make sense to use a <a href="https://en.bitcoin.it/wiki/Units">unit</a> like microbitcoin aka bits (One Millionth of a Bitcoin) and Satoshis (One hundredth of a bit). Then a bit will be a Dollar and a Satoshi will be a Cent.</p>
-          </Col>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={12}>
+              <h2>Why would it grow?</h2>
+              <p>Bitcoin is scarce. There will only be 21 Million BTC. If every Millionaire in the world wants one, there are not enough for every one to have a whole BTC.</p>
+              <p>It will make sense to use a <a href="https://en.bitcoin.it/wiki/Units">unit</a> like microbitcoin aka bits (One Millionth of a Bitcoin) and Satoshis (One hundredth of a bit). Then a bit will be a Dollar and a Satoshi will be a Cent.</p>
+            </Col>
         </Row>
 
         <Row>
