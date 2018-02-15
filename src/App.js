9477a4d76d079c23377908d2c48dfb88ca97886f
Raumi75 <@raumi75>
2018-02-15 08:55:12 +0100

    ADDED: Unit microbitcoin kilocalorie analogy

--- src/App.js
+++ src/App.js
@@ -296,6 +296,7 @@ class App extends Component {
             <p>You can buy and spend fractions of a bitcoin.</p>
             <p>Sooner or later, it will make sense to use a <a href="https://en.bitcoin.it/wiki/Units">unit</a> like microbitcoin aka bits (One Millionth of a Bitcoin) and Satoshis (One hundredth of a bit). Then a bit will be a Dollar and a Satoshi will be a Cent.</p>
             <p>How does something like <quote>'1 μBTC is 1.07 $'</quote> or <quote>'1 Satoshi is 0.01 $'</quote> sound to you? A lot less expensive, right?</p>
+            <p>Maybe people will start calling a microbitcoin just bitcoin. No big deal. When we say calorie, we actually mean a kilocalorie.</p>
           </Col>
         </Row>
 
