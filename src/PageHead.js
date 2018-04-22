0b4542390b522ce5891d3aa864b952945d5b1c97
Raumi75 <@raumi75>
2018-04-22 21:58:40 +0200

    CLEANUP: Image component not used in PageHead

--- src/PageHead.js
+++ src/PageHead.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import { Row , Col, Image } from 'react-bootstrap';
+import { Row , Col } from 'react-bootstrap';
 import {formatDollar} from './formatting.js';
 import './PageHead.css';
 
