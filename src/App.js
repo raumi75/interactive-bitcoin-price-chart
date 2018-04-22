516170e4a6b5c7d10dd29b359558a4b6ed6df52a
Raumi75 <@raumi75>
2018-04-22 18:54:20 +0200

    REFACTORED: PageFoot into separate File

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ import 'rc-slider/assets/index.css';
 import {formatDollar} from './formatting.js';
 import {getParameterByName} from './getparameter.js';
 import {getDataBoundaries} from './chartDataBoundaries.js';
+import PageFoot from './PageFoot.js';
 var Latex = require('react-latex');
 var DatePicker = require("react-bootstrap-date-picker");
 
-const donate_btc_address = "3B19wMMJD7Xjf9ajW2oRcfVfKjRprWmGrG";
 var predictionCount = 1263;   // days startDate to targetDate (2020-12-31)
 var offsetPrediction = -2389; // days startDate to minHistoricalStart
 const minSliderDistance = 29;
@@ -729,20 +729,7 @@ class App extends Component {
           </Col>
         </Row>
 }
-        <Row className="footer">
-          <Col xs={12}>
-            <p>Source: <a href="https://github.com/raumi75/mcafeetracker">raumi75@github</a></p>
-            <p>Get in touch: <a href="https://reddit.com/u/raumi75/">/u/raumi75</a>, <a href="https://twitter.com/raumi75">@raumi75</a></p>
-            <p>Spread the word:
-              <a href="https://twitter.com/raumi75/status/964468501657391104">retweet</a>,
-              share <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A//fnordprefekt.de">on Facebook</a>,
-              or on <a href="https://plus.google.com/share?url=https%3A//fnordprefekt.de">Google Plus</a>
-            </p>
-            <p>Do you like this site? Buy my kids some ice cream!</p>
-            <p><a href={"bitcoin:"+donate_btc_address}><Image src="/donate_qr.png" alt="QR-Code Donate Bitcoin" /></a></p>
-            <p><a href={"bitcoin:"+donate_btc_address}>{donate_btc_address}</a></p>
-          </Col>
-        </Row>
+        <PageFoot />
       </Grid>
     );
   }
