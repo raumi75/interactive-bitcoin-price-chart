a9e29f064b658fb40deb6c6403baa61ca448e8da
Raumi75 <@raumi75>
2018-04-23 22:21:56 +0200

    ADDED: Screenshot of first McAfee Tweet

--- src/ExplainMcAfeeTweet.js
+++ src/ExplainMcAfeeTweet.js
@@ -8,13 +8,19 @@ class ExplainMcAfeeTweet extends Component {
     const {startPrice} = this.props;
     return (
       <Row>
-        <Col xs={12} md={6}>
+        <Col xs={12}>
           <h2>It started with a tweet</h2>
+        </Col>
+        <Col xs={12} md={6}>
           <p className="lead explanation">
-            John McAfee made a bet on July 17th 2017: One single Bitcoin would be worth { formatDollar(500000) } in three years. The closing price according to CoinDesk was { formatDollar(startPrice, 3) } that day. He later revised his bet and <a href="https://twitter.com/officialmcafee/status/935900326007328768">predicted $ 1 million by the end of 2020</a>.
+            John McAfee made a bet on July 17th 2017: One single Bitcoin would be worth { formatDollar(500000) } in three years.
+            The closing price according to CoinDesk was { formatDollar(startPrice, 3) } that day.
+            <strong> Look what is at stake:</strong>
+            <a href="https://twitter.com/officialmcafee/status/887024683379544065"><Image src="tweet20170717.png" responsive /></a>
           </p>
         </Col>
         <Col xs={12} md={6}>
+          <p className="lead">He later <strong>revised</strong> his bet and <a href="https://twitter.com/officialmcafee/status/935900326007328768">predicted $ 1 million by the end of 2020</a>.</p>
           <p><a href="https://twitter.com/officialmcafee/status/935900326007328768"><Image src="tweet20171129.png" responsive /></a></p>
         </Col>
       </Row>
