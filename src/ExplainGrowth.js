d93ef79818c8849ce1e60dde4b8433ca01b10bd6
Raumi75 <@raumi75>
2018-04-25 10:31:29 +0200

    REFACTORED: Moved explanations into separate files

---
+++ src/ExplainGrowth.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Row , Col } from 'react-bootstrap';
+
+export default class ExplainGrowth extends Component {
+
+render() { return (
+  <div>
+    <h2>That parabolic curve - seriously?</h2>
+    <p>When you zoom out, the curve gets steeper and steeper.
+    Relax! Growth curves look like that. Take the Dow Jones, a savings account
+    with interest or bacteria growing in a petri dish.</p>
+    <p>With a fixed percentage per time, rising from 1 to 10 takes as long as
+    from 100,000 to 1,000,000. Both is the growth by a factor of 10.</p>
+    <p>That is why many analysts like to look at charts with a <strong>logarithmic
+    scale</strong> where the y-axis scales in orders of magnitude.</p>
+  </div>
+    );
+  }
+}
---
+++ src/ExplainUnit.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Row , Col } from 'react-bootstrap';
+import {formatDollar} from './formatting.js';
+
+export default class ExplainUnit extends Component {
+
+render() { return (
+  <div>
+    <h2>Such an expensive currency!</h2>
+    <p>You can buy and spend fractions of a bitcoin.</p>
+    <p>Sooner or later, it will make sense to use a <a href="https://en.bitcoin.it/wiki/Units">unit</a> like microbitcoin aka bits (One Millionth of a Bitcoin) and Satoshis (One hundredth of a bit). Then a bit will be a Dollar and a Satoshi will be a Cent.</p>
+    <p>How does something like <em>'1 μBTC is {formatDollar(1.07)}'</em> or <em>'1 Satoshi is {formatDollar(0.01)}'</em> sound to you? A lot less expensive, right?</p>
+    <p>Maybe people will start calling a microbitcoin just bitcoin. No big deal. When we say calorie, we actually mean a kilocalorie.</p>
+  </div>
+    );
+  }
+}
