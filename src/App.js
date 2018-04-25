ba1efb5acc22d15807598f09f5275eb940d4f46b
Raumi75 <@raumi75>
2018-04-25 09:37:20 +0200

    ADDED: explain energy consumption

--- src/App.js
+++ src/App.js
@@ -565,6 +565,35 @@ class App extends Component {
           </Col>
         </Row>
 
+        <Row>
+          <Col xs={12} md={10} mdOffset={1} lg={8} lgOffset={2}>
+            <h2>Is bitcoin wasting electricity?</h2>
+
+            <p>Mining bitcoin is hard <strong>on purpose</strong>.
+              The so called <strong>proof of work</strong> is what controls the money supply
+              and what makes the ledger tamper resistant.</p>
+            <p>Bitcoin is both a currency and a ledger for storing all transactions.
+              Gold would not have much value if it were easy to find or if it were cheap to create.
+              A ledger that was easy to tamper with would be worthless.</p>
+            <p>The bitcoin ledger is called blockchain and each entry is called a block.
+              Supercomputers all over the world are competing to find the next block.
+              The winner gets the so called block reward and the transaction fees.
+              On average they find one every 10 minutes.</p>
+            <p>All these miners do not trust each other. They reject invalid blocks and
+              attempts to rewrite history. Someone trying to tamper with the ledger would
+              need an incredible amount of computing power and electricity to overpower
+              all the others.</p>
+
+            <p>What about CO<sub>2</sub>-emissions? Many miners are located in places with a
+              surplus of electricity (geothermal power in Iceland, hydroelectric or solar power
+              in the middle of nowhere). It is cheaper to send a bitcoin-block over the internet
+              than building more power lines.</p>
+
+            <p>Still not convinced? Think for a minute how complicated the current
+              fiat banking system is and you will agree that bitcoin is a lot more effient.</p>
+          </Col>
+        </Row>
+
         { !this.state.customPrediction ?
 
         <Row>
