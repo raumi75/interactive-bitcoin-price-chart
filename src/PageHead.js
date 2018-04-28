9d9261b423ef3adcf89b800b0db1d623832c9918
Raumi75 <@raumi75>
2018-04-28 21:33:42 +0200

    REFACTORED: Title in pagehead in separate function

--- src/PageHead.js
+++ src/PageHead.js
@@ -4,33 +4,45 @@ import { Row , Col } from 'react-bootstrap';
 import {formatDollar} from './formatting.js';
 import './PageHead.css';
 
-class PageHead extends Component {
+export default class PageHead extends Component {
 
-  render() {
+  predictionTitle() {
     const {customPrediction, targetPrice, targetDate} = this.props;
 
-    return (
-    <div>
-    <Row className="about">
-      <Col xs={12}>
-        <small>made by a bitcoin fan - <a href="#pagefoot">Feedback or tips</a>.</small>
-      </Col>
-    </Row>
-
-    <Row className="header">
-      <Col xs={12}>
-        <h1 className="header">Bitcoin Price Prediction Tracker</h1>
-        { customPrediction ?
-          <p>Will bitcoin be {formatDollar(targetPrice)} on {moment(targetDate).format('MMMM Do YYYY')}?</p>
-        :
-          <p>John McAfee says: By the end of 2020 Bitcoin will be worth $&nbsp;1&nbsp;Million. Is he losing his bet?</p>
-        }
-      </Col>
-    </Row>
-    </div>
-    );
+    if (customPrediction) {
+      return (
+        <p>
+          Will bitcoin be {formatDollar(targetPrice)}
+          on {moment(targetDate).format('MMMM Do YYYY')}?
+        </p>
+      )
+    } else {
+      return (
+        <p>
+          John McAfee says:
+          By the end of 2020 Bitcoin will be worth $&nbsp;1&nbsp;Million.
+          Is he losing his bet?
+        </p>
+      );
+    }
   }
 
-}
+  render() {
+    return (
+      <div>
+        <Row className="about">
+          <Col xs={12}>
+            <small>made by a bitcoin fan - <a href="#pagefoot">Feedback or tips</a>.</small>
+          </Col>
+        </Row>
 
-export default PageHead;
+        <Row className="header">
+          <Col xs={12}>
+            <h1 className="header">Bitcoin Price Prediction Tracker</h1>
+            {this.predictionTitle()}
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
