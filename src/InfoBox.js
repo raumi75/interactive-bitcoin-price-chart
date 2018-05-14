94a514a754c45fb424ed59641fdae12fca621552
Raumi75 <@raumi75>
2018-05-14 07:43:51 +0200

    REFORMATTING: smaller font for displaying age of price in seconds during network trouble

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -45,7 +45,7 @@ class InfoBox extends Component {
           <div className="heading actual"><a href="https://www.coindesk.com/price/" title="Powered by coindesk">{formatDollar(actualPriceNow)}</a></div>
           {(PriceAgeSeconds > 120 && PriceAgeSeconds < 60*60*24)
             ?
-              <div className="subtext">last connection {PriceAgeSeconds} sec</div>
+              <div className="subtext"><small>{PriceAgeSeconds} seconds ago. connecting...</small></div>
             :
             <ProgressBar className="actualupdatesin" now={actualUpdatesIn} max={60} />
           }
