384ea73b8616749ad9289df10a94a2af04e1d9b2
Raumi75 <@raumi75>
2018-07-25 07:39:14 +0200

    ADDED: Twitter account links again. Doesn't seem like I'm banned any more.

--- src/PageFoot.js
+++ src/PageFoot.js
@@ -13,13 +13,14 @@ class PageFoot extends Component {
         <p>Data-Source: <strong>Powered by <a href="https://www.coindesk.com/price/">CoinDesk</a></strong></p>
         <p>Chart based on Brandon Morelli's <a href="https://codeburst.io/how-i-built-an-interactive-30-day-bitcoin-price-graph-with-react-and-an-api-6fe551c2ab1d">Tutorial</a></p>
         <p>Source: <a href="https://github.com/raumi75/mcafeetracker">raumi75@github</a></p>
-        <p><strong>Suggestions, Feedback, Ideas?</strong> Get in touch: <a href="https://reddit.com/u/raumi75/">/u/raumi75</a> on reddit</p>
+        <p><strong>Suggestions, Feedback, Ideas?</strong> Get in touch: <a href="https://reddit.com/u/raumi75/">/u/raumi75</a>, <a href="https://twitter.com/raumi75">@raumi75</a></p>
         <p>
           Spread the word:
           share on <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A//fnordprefekt.de">Facebook</a>,
           or on <a href="https://plus.google.com/share?url=https%3A//fnordprefekt.de">Google Plus</a>
         </p>
         <p>Do you love this site? It's ad-free. Tips are welcome.</p>
+        <p>Follow me on <a href="https://twitter.com/raumi75">Twitter (@raumi75)</a> for daily price updates.</p>
         <DonateQr btc_address="3B19wMMJD7Xjf9ajW2oRcfVfKjRprWmGrG" />
       </Col>
     </Row>
