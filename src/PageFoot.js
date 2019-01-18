5136da37e8bf5dd264de9a63704934bacee1e7d9
Raumi75 <@raumi75>
2019-01-18 12:14:49 +0100

    ADDED: Link to my tippin.me for accepting lightning donations

--- src/PageFoot.js
+++ src/PageFoot.js
@@ -22,6 +22,8 @@ class PageFoot extends Component {
         <p>Follow me on <a href="https://twitter.com/raumi75">Twitter (@raumi75)</a> for daily price updates.</p>
         <p>Do you love this site? It's open-source, ad-free, anonymous, cookie-free and tracker-free. Tips are welcome.</p>
         <DonateQr btc_address="3B19wMMJD7Xjf9ajW2oRcfVfKjRprWmGrG" />
+        <p>Or use this button to</p>
+        <p><a href="https://tippin.me/@raumi75" className="btn btn-danger btn-lg">Tip me via ⚡️Lightning Network</a></p>
       </Col>
     </Row>
     );
