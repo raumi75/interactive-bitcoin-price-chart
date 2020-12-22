1303c5faf1b3e0c11b40da5c86b697b161a09f30
Raumi75 <@raumi75>
2020-12-22 09:57:19 +0100

    FIXED: load closing price for tweet

--- extras/mcafeetweet.sh
+++ extras/mcafeetweet.sh
@@ -3,8 +3,16 @@
 # One day later than tweetDate because we want yesterdays price
 LC_NUMERIC="en_US.UTF-8"
 predictedprice=$(echo "scale=2; 1.00484095526^"$(( ($(date +%s) - $(date --date="2017-07-18" +%s) )/(60*60*24) ))"*224427/100" | bc)
+closingprice=$(printf %.2f $(curl -s https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday | jq '.bpi' | jq .[]))
+#closingprice=$(printf %.2f $(curl -s https://api.coindesk.com/v1/bpi/currentprice.json | python -c "import json, sys; print(json.load(sys.stdin)['bpi']['USD']['rate'])" | tr -d ','))
+#closingprice=$(printf %.2f $(curl -s https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday | python -c "import json, sys; print(json.load(sys.stdin)['bpi']['USD']['rate'])" | tr -d ','))
 
-closingprice=$(printf %.2f $(curl -s https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday | jq '.bpi' | jq '.[] | tonumber'))
+predictedprice_formatted=$(printf "%'.2f" $predictedprice)
+
+if (( $(echo "$closingprice > 0" |bc -l) ));
+then
+echo 'ok: price loaded';
+
 percentage=$(printf %.2f $(echo "scale=4; $float($closingprice / $predictedprice -1)*100" | bc -l))
 # No idea why this is negated.
 if (( $(echo "$predictedprice < $closingprice" | bc -l) ));
@@ -12,10 +20,15 @@ if (( $(echo "$predictedprice < $closingprice" | bc -l) ));
   else aboveorbelow='below';
 fi;
 
-predictedprice_formatted=$(printf "%'.2f" $predictedprice)
+
 closingprice_formatted=$(printf "%'.2f" $closingprice)
 
-tweettext=$(echo "Daily update: McAfee #dickline was $" $predictedprice_formatted 'at midnight UTC while #bitcoin price was $' $closingprice_formatted ' ('${percentage#-}'%' $aboveorbelow'). See the chart and learn more on https://dickline.info $btc')
+tweettext=$(echo "Daily update: McAfee prediction was $" $predictedprice_formatted 'at midnight UTC while #bitcoin price was $' $closingprice_formatted ' ('${percentage#-}'%' $aboveorbelow'). See the chart and learn more on https://bircoin.top $btc')
+
+else
+    echo 'warning: no price loaded';
+    tweettext=$(echo "Daily update: McAfee #bitcoin price prediction was $" $predictedprice_formatted 'at midnight UTC. See the chart and learn more on https://bircoin.top $btc')
+fi;
 
 /home/jan/.rbenv/shims/t update "$tweettext"
 
